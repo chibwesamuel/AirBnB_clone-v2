ac11ec04c52786ac2eae099ab6b2c78fdf41ebd7
chibwesamuel <smchibwe@gmail.com>
2023-04-21 03:31:35 -0700

    Installed Flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,36 +4,17 @@ starts a Flask web application
 """
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
-root@5106a7549cc0:/clone3bnb/web_flask# cat 1-hbnb_route.py
-#!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
-
+def hello():
+    return ("Hello HBNB")
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+    return ("HBNB")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
