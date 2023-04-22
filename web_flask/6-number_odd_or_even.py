68c6f07137e1566c0e2b35738416cfc139add21b
chibwesamuel <smchibwe@gmail.com>
2023-04-22 12:05:21 -0700

    New take on 5 & 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -37,12 +37,12 @@ def imanumber(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
 	"""display a HTML page only if n is an integer"""
-	return (render_template('5-number.html', n=n))
+	return (render_template('number.html', n=n))
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """display a HTML page only if n is an integer"""
-    return (render_template('6-number_odd_or_even.html', n=n))
+    return (render_template('odd_or_even.html', n=n))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
